ed03f4e4edcdbded56d232994a193477591244e1
Tom Watkinson <tomwatkinson90@gmail.com>
2019-07-27 21:42:06 +0100

    added materials to lengths form

--- index.php
+++ index.php
@@ -6,5 +6,10 @@
 </head>
 <body>
 <h1>Fence Length Calculator</h1>
+<h2>Materials -> Length</h2>
+<p>Please enter the number of posts and rails you have:</p>
+<form action="index.php" method="get">
+    <p>Posts: <input type="number" name="posts"> Rails: <input type="number" name="rails"</p>
+</form>
 </body>
 </html>
