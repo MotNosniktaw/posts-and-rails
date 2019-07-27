b9420d15bc5a111ee8ee92adb83392cf507cb13d
Tom Watkinson <tomwatkinson90@gmail.com>
2019-07-27 21:46:40 +0100

    - added Length to Materials form - addes submit buttons to forms

--- index.php
+++ index.php
@@ -9,7 +9,12 @@
 <h2>Materials -> Length</h2>
 <p>Please enter the number of posts and rails you have:</p>
 <form action="index.php" method="get">
-    <p>Posts: <input type="number" name="posts"> Rails: <input type="number" name="rails"</p>
+    <p>Posts: <input type="number" name="posts"> Rails: <input type="number" name="rails"> <input type="submit" name="materials"</p>
+</form>
+<h2>Length -> Materials</h2>
+<p>Please enter the length of the fence you need to build:</p>
+<form action="index.php" method="get">
+    <p>Length: <input type="number" name="fence"> <input type="submit" name="length"></p>
 </form>
 </body>
 </html>
