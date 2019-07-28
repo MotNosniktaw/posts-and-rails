f2bb6a019632fdfe01429628bd92085b8081fa83
Tom Watkinson <tomwatkinson90@gmail.com>
2019-07-28 16:12:24 +0100

    input fields keep entered values on submit

--- index.php
+++ index.php
@@ -14,12 +14,12 @@ $newFence = new Fence();
 <h2>Materials -> Length</h2>
 <p>Please enter the number of posts and rails you have:</p>
 <form action="index.php" method="get">
-    <p>Posts: <input type="number" name="posts"> Rails: <input type="number" name="rails"> <input type="submit" name="materials"</p>
+    <p>Posts: <input type="number" value="<?php echo $_GET['posts']?>" name="posts"> Rails: <input type="number" value="<?php echo $_GET['rails']?>" name="rails"> <input type="submit" name="materials"</p>
 </form>
 <h2>Length -> Materials</h2>
 <p>Please enter the length of the fence you need to build:</p>
 <form action="index.php" method="get">
-    <p>Length: <input type="number" step="0.01" name="fence"> <input type="submit" name="length"></p>
+    <p>Length: <input type="number" step="0.01" value="<?php echo $_GET['fence']?>" name="fence"> <input type="submit" name="length"></p>
 </form>
 <br>
 <p><?php
