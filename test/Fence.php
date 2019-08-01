203396d0447c63add26905e27e7e0a4f44654a7b
Tom Watkinson <tomwatkinson90@gmail.com>
2019-08-01 16:44:44 +0100

    added length calc tests for more rails, more posts and invlaid input

--- test/Fence.php
+++ test/Fence.php
@@ -1,5 +1,5 @@
 <?php
-require '../Fence.php';
+require_once '../Fence.php';
 
 use PHPUnit\Framework\TestCase;
 
@@ -10,10 +10,43 @@ Class FenceCalcTest extends TestCase
         // Setup
         $posts = 7;
         $rails = 45354;
-        $expectedOutcome = 'The materials you have provided will build a fence that is 10.6m long.'
+        $newFence = new Fence();
+        $expectedOutcome = 'The materials you have provided will build a fence that is 10.6m long.';
 
         // Execution
-        $actualOutcome = buildFence($posts, $rails);
+        $actualOutcome = $newFence->buildFence($posts, $rails);
+
+        // Assertions
+        $this->assertEquals($expectedOutcome, $actualOutcome);
+
+    }
+
+    public function testLengthCalcMorePosts()
+    {
+        // Setup
+        $posts = 765;
+        $rails = 46;
+        $newFence = new Fence();
+        $expectedOutcome = 'The materials you have provided will build a fence that is 69.1m long.';
+
+        // Execution
+        $actualOutcome = $newFence->buildFence($posts, $rails);
+
+        // Assertions
+        $this->assertEquals($expectedOutcome, $actualOutcome);
+
+    }
+
+    public function testLengthCalcInvalidInput()
+    {
+        // Setup
+        $posts = 'XX';
+        $rails = '';
+        $newFence = new Fence();
+        $expectedOutcome = 'To use the Fence Length Calculator function, please enter number values greater than 0.';
+
+        // Execution
+        $actualOutcome = $newFence->buildFence($posts, $rails);
 
         // Assertions
         $this->assertEquals($expectedOutcome, $actualOutcome);
