20dbc0d66d89d84a25fe19e2fa37809d9b8bc246
Tom Watkinson <tomwatkinson90@gmail.com>
2019-08-01 16:28:50 +0100

    - added test class for length calculator

--- test/Fence.php
+++ test/Fence.php
@@ -3,3 +3,20 @@ require '../Fence.php';
 
 use PHPUnit\Framework\TestCase;
 
+Class FenceCalcTest extends TestCase
+{
+    public function testLengthCalcMoreRails()
+    {
+        // Setup
+        $posts = 7;
+        $rails = 45354;
+        $expectedOutcome = 'The materials you have provided will build a fence that is 10.6m long.'
+
+        // Execution
+        $actualOutcome = buildFence($posts, $rails);
+
+        // Assertions
+        $this->assertEquals($expectedOutcome, $actualOutcome);
+
+    }
+}
